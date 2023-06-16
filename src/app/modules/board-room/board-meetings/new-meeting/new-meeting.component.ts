f4728ac4f115513a0498316e12e6778d83931532
Adrian Zinko <zinko.adrian00@gmail.com>
2023-06-16 20:05:44 +0200

    Merge pull request #1 from adrianghub/develop
    initial setup

---
+++ src/app/modules/board-room/board-meetings/new-meeting/new-meeting.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'koia-new-meeting',
+  templateUrl: './new-meeting.component.html',
+  styleUrls: ['./new-meeting.component.scss'],
+})
+export class NewMeetingComponent {}
